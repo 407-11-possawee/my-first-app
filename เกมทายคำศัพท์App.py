df8aae8b11ede8d773b4536092bc85d2c17ce368
407-11-possawee <54206@yupparaj.ac.th>
2026-09-10 22:05:23 +0700

    Update เกมทายคำศัพท์App.py

--- เกมทายคำศัพท์App.py
+++ เกมทายคำศัพท์App.py
@@ -60,14 +60,14 @@ def show_result_dialog(ans1, ans2, ans3, ans4, ans5):
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
      # ตรวจข้อ 4
-    if u_ans4 == "Table":
+    if u_ans4 == "Soap":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
 
       # ตรวจข้อ 5
-    if u_ans5 == "Table":
+    if u_ans5 == "Ruler":
         st.success("✅ ข้อ 5: ถูกต้อง")
         score += 1
     else:
